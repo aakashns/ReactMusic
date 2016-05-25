1d97a828dba20659bc36fd6431775bdbd53daaab
Aakash N S <aakashns.sky@gmail.com>
2016-05-25 16:24:28 +0530

    Added and linked react-native-video

--- android/app/src/main/java/com/reactmusic/MainActivity.java
+++ android/app/src/main/java/com/reactmusic/MainActivity.java
@@ -1,6 +1,7 @@
 package com.reactmusic;
 
 import com.facebook.react.ReactActivity;
+import com.brentvatne.RCTVideo.ReactVideoPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +35,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new ReactVideoPackage()
         );
     }
 }
